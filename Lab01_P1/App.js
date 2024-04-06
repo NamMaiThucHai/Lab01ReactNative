72dea90125ae823f1728145767c48fcbcacdede0
NamMaiThucHai <134347252+NamMaiThucHai@users.noreply.github.com>
2024-04-06 21:42:06 +0700

    Add files via upload

---
+++ Lab01_P1/App.js
@@ -0,0 +1,16 @@
+import { View } from "react-native"
+import Bai1 from "./src/bai1";
+import Bai2 from "./src/bai2";
+import Bai3 from "./src/bai3";
+import Bai4 from "./src/bai4";
+import Bai5 from "./src/bai5";
+import Bai6 from "./src/bai6";
+import Bai7 from "./src/bai7";
+import Bai8 from "./src/bai8";
+
+const App =()=>{
+  return(
+      <Bai8/>
+  )
+}
+export default App;
